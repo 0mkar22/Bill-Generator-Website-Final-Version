78613c3e1dcfc6d4c02f34355d1d54fe6c22b847
0mkar22 <obhogate48@gmail.com>
2026-08-03 22:42:47 +0530

    fixing the CROS issue

--- bill-generator-app/server/index.js
+++ bill-generator-app/server/index.js
@@ -15,13 +15,7 @@ const allowedOrigins = (process.env.CORS_ORIGIN || 'http://localhost:5173')
     .filter(Boolean);
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true
 }));
 
