1cd60665c6fafe209d51ecf0d018e0a6130f1d9f
0mkar22 <obhogate48@gmail.com>
2026-09-01 22:04:47 +0530

    feat: add edit delete and filtering for personnel payouts

--- bill-generator-app/server/routes/personnelPayouts.js
+++ bill-generator-app/server/routes/personnelPayouts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getPayouts, createPayout, deletePayout } = require('../controllers/personnelPayouts');
+const { getPayouts, createPayout, deletePayout, updatePayout } = require('../controllers/personnelPayouts');
 
 router.route('/')
   .get(getPayouts)
   .post(createPayout);
 
 router.route('/:id')
+  .put(updatePayout)
   .delete(deletePayout);
 
 module.exports = router;
